90317cd6a9b94caedccfa12d96f63c74b9978121
Ayush Tripathi <ayushtripathi1012@gmail.com>
2024-03-30 22:35:14 +0530

    131

---
+++ 131. Palindrome Partitioning.py
@@ -0,0 +1,12 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        def dfs(l, final):
+            if not l:
+                res.append(final)
+                return
+            for i in range(len(l)):
+                if l[:i+1] == l[:i+1][::-1]:
+                    dfs(l[i+1:],final + [l[:i+1]])
+        dfs(s,[])
+        return res
